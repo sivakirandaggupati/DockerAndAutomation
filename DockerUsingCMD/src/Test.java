e8740245f2fd871165e052d3ceb1234f518ddd09
sivakirandaggupati <sivakiran.daggupati@outlook.com>
2025-10-26 14:35:08 +1100

    My first Docker commit
    This is my first docker work commit

---
+++ DockerUsingCMD/src/Test.java
@@ -0,0 +1,7 @@
+class Test {
+public static void main(String[] args) {
+     System.out.println("Hey there.");
+     AddNumbers x = new AddNumbers();
+     x.addnumbers(2, 3);
+}
+}
---
+++ DockerUsingJar/AddNumbers.java
@@ -0,0 +1,7 @@
+public class AddNumbers {
+    
+    public void addnumbers(int i,int j) {
+        int x=i+j;
+        System.out.println(x);
+    }
+}
